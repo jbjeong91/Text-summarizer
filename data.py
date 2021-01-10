4e97981e4931a66bd90d761d4d44bf8584b5f55e
Jeongbeom Jeong <55580548+jbjeong91@users.noreply.github.com>
2021-01-10 23:07:46 +0900

    Update data.py

--- data.py
+++ data.py
@@ -1,7 +1,8 @@
 #from konlpy.tag import Twitter
 from nltk.tokenize import WordPunctTokenizer
 import pandas as pd
-import tensorflow as tf
+#import tensorflow as tf
+import tensorflow.compat.v1 as tf
 import enum
 import os
 import re
@@ -10,7 +11,9 @@ import numpy as np
 from configs import DEFINES
 
 from tqdm import tqdm
-
+#--------------
+from bs4 import BeautifulSoup
+#----------------
 FILTERS = "([~.,\"':;)(]<>)'...'"
 PAD = "<PAD>"
 STD = "<SOS>"
@@ -26,6 +29,8 @@ MARKER = [PAD, STD, END, UNK]
 #CHANGE_FILTER = re.compile(FILTERS)
 CHANGE_FILTER = "[...]|[..]|[.]|,|<|>|br|/|-|!"
 
+contractions = {"ain't": "is not", "aren't": "are not","can't": "cannot", "'cause": "because", "could've": "could have", "couldn't": "could not", "didn't": "did not",  "doesn't": "does not", "don't": "do not", "hadn't": "had not", "hasn't": "has not", "haven't": "have not", "he'd": "he would","he'll": "he will", "he's": "he is", "how'd": "how did", "how'd'y": "how do you", "how'll": "how will", "how's": "how is", "I'd": "I would", "I'd've": "I would have", "I'll": "I will", "I'll've": "I will have","I'm": "I am", "I've": "I have", "i'd": "i would", "i'd've": "i would have", "i'll": "i will",  "i'll've": "i will have","i'm": "i am", "i've": "i have", "isn't": "is not", "it'd": "it would", "it'd've": "it would have", "it'll": "it will", "it'll've": "it will have","it's": "it is", "let's": "let us", "ma'am": "madam", "mayn't": "may not", "might've": "might have","mightn't": "might not","mightn't've": "might not have", "must've": "must have", "mustn't": "must not", "mustn't've": "must not have", "needn't": "need not", "needn't've": "need not have","o'clock": "of the clock", "oughtn't": "ought not", "oughtn't've": "ought not have", "shan't": "shall not", "sha'n't": "shall not", "shan't've": "shall not have", "she'd": "she would", "she'd've": "she would have", "she'll": "she will", "she'll've": "she will have", "she's": "she is", "should've": "should have", "shouldn't": "should not", "shouldn't've": "should not have", "so've": "so have","so's": "so as", "this's": "this is","that'd": "that would", "that'd've": "that would have", "that's": "that is", "there'd": "there would", "there'd've": "there would have", "there's": "there is", "here's": "here is","they'd": "they would", "they'd've": "they would have", "they'll": "they will", "they'll've": "they will have", "they're": "they are", "they've": "they have", "to've": "to have", "wasn't": "was not", "we'd": "we would", "we'd've": "we would have", "we'll": "we will", "we'll've": "we will have", "we're": "we are", "we've": "we have", "weren't": "were not", "what'll": "what will", "what'll've": "what will have", "what're": "what are", "what's": "what is", "what've": "what have", "when's": "when is", "when've": "when have", "where'd": "where did", "where's": "where is", "where've": "where have", "who'll": "who will", "who'll've": "who will have", "who's": "who is", "who've": "who have", "why's": "why is", "why've": "why have", "will've": "will have", "won't": "will not", "won't've": "will not have", "would've": "would have", "wouldn't": "would not", "wouldn't've": "would not have", "y'all": "you all", "y'all'd": "you all would","y'all'd've": "you all would have","y'all're": "you all are","y'all've": "you all have", "you'd": "you would", "you'd've": "you would have", "you'll": "you will", "you'll've": "you will have", "you're": "you are", "you've": "you have"}
+
 def load_data():
     # 판다스를 통해서 데이터를 불러온다.
     data_df = pd.read_csv(DEFINES.data_path, header=0)
@@ -38,13 +43,26 @@ def load_data():
     # 그 값을 리턴한다.
     return train_input, train_label, eval_input, eval_label
 
-# 지저분한 리뷰 문장 전처리
+'''
 def prepro_seq(seq):
     seq = str(seq)
     seq = re.sub(CHANGE_FILTER, " ", seq)
     seq = re.sub(" +", " ", seq)
     return seq
+'''
+# 지저분한 리뷰 문장 전처리
+def preprocess_sentence(sentence):
+    sentence = sentence.lower() # 텍스트 소문자화
+    sentence = BeautifulSoup(sentence, "lxml").text # <br />, <a href = ...> 등의 html 태그 제거
+    sentence = re.sub(r'\([^)]*\)', '', sentence) # 괄호로 닫힌 문자열  제거 Ex) my husband (and myself) for => my husband for
+    sentence = re.sub('"','', sentence) # 쌍따옴표 " 제거
+    sentence = ' '.join([contractions[t] if t in contractions else t for t in sentence.split(" ")]) # 약어 정규화
+    sentence = re.sub(r"'s\b","",sentence) # 소유격 제거. Ex) roland's -> roland
+    sentence = re.sub("[^a-zA-Z]", " ", sentence) # 영어 외 문자(숫자, 특수문자 등) 공백으로 변환
+    sentence = re.sub('[m]{2,}', 'mm', sentence) # m이 3개 이상이면 2개로 변경. Ex) ummmmmmm yeah -> umm yeah
 
+    tokens = ' '.join(word for word in sentence.split() if len(word) > 1)
+    return tokens
 
 def prepro_like_morphlized(data):
     # 형태소 분석 모듈 객체를
@@ -60,7 +78,7 @@ def prepro_like_morphlized(data):
         # Twitter.morphs 함수를 통해 토크나이즈 된
         # 리스트 객체를 받고 다시 공백문자를 기준으로
         # 하여 문자열로 재구성 해줍니다.
-        seq = prepro_seq(seq)
+        seq = preprocess_sentence(seq)
         morphlized_seq = " ".join(WP_analyzer.tokenize(seq))
         result_data.append(morphlized_seq)
 
@@ -86,7 +104,7 @@ def enc_processing(value, dictionary):
         # 정규화를 사용하여 필터에 들어 있는
         # 값들을 "" 으로 치환 한다.
         #sequence = re.sub(CHANGE_FILTER, "", sequence)
-        sequence = prepro_seq(sequence)
+        sequence = preprocess_sentence(sequence)
         # 하나의 문장을 인코딩 할때
         # 가지고 있기 위한 배열이다.
         sequence_index = []
@@ -137,7 +155,7 @@ def dec_output_processing(value, dictionary):
         # FILTERS = "([~.,!?\"':;)(])"
         # 정규화를 사용하여 필터에 들어 있는
         # 값들을 "" 으로 치환 한다.
-        sequence = prepro_seq(sequence)
+        sequence = preprocess_sentence(sequence)
         #sequence = re.sub(CHANGE_FILTER, "", sequence)
         #sequence = str(sequence)
         # 하나의 문장을 디코딩 할때 가지고
@@ -182,7 +200,7 @@ def dec_target_processing(value, dictionary):
         # 값들을 "" 으로 치환 한다.
         #sequence = re.sub(CHANGE_FILTER, "", sequence)
         #sequence = str(sequence)
-        sequence = prepro_seq(sequence)
+        sequence = preprocess_sentence(sequence)
         # 문장에서 스페이스 단위별로 단어를 가져와서
         # 딕셔너리의 값인 인덱스를 넣어 준다.
         # 디코딩 출력의 마지막에 END를 넣어 준다.
@@ -327,7 +345,7 @@ def data_tokenizer(data):
     # 토크나이징 해서 담을 배열 생성
     words = []
     for sentence in data:
-        sentence = prepro_seq(sentence)
+        sentence = preprocess_sentence(sentence)
         #print('정체크',sentence)
         # FILTERS = "([~.,!?\"':;)(])"
         # 위 필터와 같은 값들을 정규화 표현식을
