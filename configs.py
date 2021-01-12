0c91bd623d8b838327d0d3a1a74d3a085ff4a74c
Jeongbeom Jeong <55580548+jbjeong91@users.noreply.github.com>
2021-01-13 00:04:32 +0900

    Update configs.py

--- configs.py
+++ configs.py
@@ -2,13 +2,13 @@
 import tensorflow as tf
 
 tf.app.flags.DEFINE_string('cmd', 'train', 'cmd')  # train or test
-tf.app.flags.DEFINE_integer('batch_size', 64, 'batch size')  # 배치 크기
+tf.app.flags.DEFINE_integer('batch_size', 128, 'batch size')  # 배치 크기
 tf.app.flags.DEFINE_integer('train_steps', 10000, 'train steps')  # 학습 에포크
 tf.app.flags.DEFINE_float('dropout_width', 0.9, 'dropout width')  # 드롭아웃 크기
 tf.app.flags.DEFINE_integer('embedding_size', 128, 'embedding size')  # 가중치 크기 # 논문 512 사용
 tf.app.flags.DEFINE_float('learning_rate', 1e-4, 'learning rate')  # 학습률
 tf.app.flags.DEFINE_integer('shuffle_seek', 1000, 'shuffle random seek')  # 셔플 시드값
-tf.app.flags.DEFINE_integer('max_sequence_length', 50, 'max sequence length')  # 시퀀스 길이
+tf.app.flags.DEFINE_integer('max_sequence_length', 60, 'max sequence length')  # 시퀀스 길이
 tf.app.flags.DEFINE_integer('model_hidden_size', 128, 'model weights size')  # 모델 가중치 크기
 tf.app.flags.DEFINE_integer('ffn_hidden_size', 512, 'ffn weights size')  # ffn 가중치 크기
 tf.app.flags.DEFINE_integer('attention_head_size', 8, 'attn head size')  # 멀티 헤드 크기
